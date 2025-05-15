715b08797ac3a0af745a02e563c82044cf5189e7
Markel-Fernandez-de-Labastida <markelfdz8@gmail.com>
2025-05-15 13:52:06 +0200

    fist commit rama develop. funcionalidad pintarBotones

--- js/main.js
+++ js/main.js
@@ -53,3 +53,81 @@
 usar filter para crear un nuevo array almacenando los tag 
 
 */
+// https://prod.liveshare.vsengsaas.visualstudio.com/join?7CBACAD50DB79E28D11D2E38839FCCF043AB
+
+
+const arrEtiquetas = [`Mar`, `Edificio`, `Señales`, `Arena`, `Cosa`];
+
+const arrViajes = [
+    {
+        titulo: "Titulo de viaje 1",
+        srcImg: "/assets/img/viajes/viajes-1.jpg",
+        alt: "viaje 1",
+        descripcion: "viaje 1",
+        tags: [`Mar`, `Arena`]
+    },
+    {
+        titulo: "Titulo de viaje 2",
+        srcImg: "/assets/img/viajes/viajes-2.jpg",
+        alt: "viaje 2",
+        descripcion: "viaje 2",
+        tags: [`Mar`, `Edificio`]
+    },
+    {
+        titulo: "Titulo de viaje 3",
+        srcImg: "/assets/img/viajes/viajes-3.jpg",
+        alt: "viaje 3",
+        descripcion: "viaje 3",
+        tags: [`Señales`]
+    },
+    {
+        titulo: "Titulo de viaje 4",
+        srcImg: "/assets/img/viajes/viajes-4.jpg",
+        alt: "viaje 4",
+        descripcion: "viaje 4",
+        tags: [`Edificio`]
+    },
+    {
+        titulo: "Titulo de viaje 5",
+        srcImg: "/assets/img/viajes/viajes-5.jpg",
+        alt: "viaje 5",
+        descripcion: "viaje 5",
+        tags: [`Edificio`]
+    },
+    {
+        titulo: "Titulo de viaje 6",
+        srcImg: "/assets/img/viajes/viajes-6.jpg",
+        alt: "viaje 6",
+        descripcion: "viaje 6",
+        tags: [`Mar`, `Arena`]
+    },
+    {
+        titulo: "Titulo de viaje 7",
+        srcImg: "/assets/img/viajes/viajes-7.jpg",
+        alt: "viaje 7",
+        descripcion: "viaje 7",
+        tags: [`Edificio`]
+    },
+]
+
+const pintarBotones = () =>  {
+    const botonesContenedorFlex = document.querySelector(".flexBotones");
+
+    arrEtiquetas.forEach((valor, index, array) => {
+        const botonesFlex = document.createElement("BUTTON");
+        botonesFlex.id = arrEtiquetas[index];
+        botonesFlex.classList.add("boton");
+        botonesFlex.innerHTML = arrEtiquetas[index];
+        console.log(botonesFlex);
+
+        botonesContenedorFlex.append(botonesFlex);
+    })
+}
+
+
+
+
+
+
+
+pintarBotones();
